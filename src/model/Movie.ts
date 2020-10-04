eaae10d1bcecd3694593de9b9bc5ac56bdabd48a
Piotr Mol <piotr94mol@gmail.com>
2020-10-04 19:10:59 +0200

    Added validation to Moview model

--- src/model/Movie.ts
+++ src/model/Movie.ts
@@ -1,16 +1,64 @@
+import { IsNotEmpty, MaxLength } from "class-validator";
 
 export default class Movie {
 
+    readonly id: number;
+
+    @IsNotEmpty({
+        message: "Title is required"
+    })
+    @MaxLength(255, {
+        message: "Title cannot be longer than 255 characters"
+    })
+    readonly title: string;
+
+    @IsNotEmpty({
+        message: "Year is required"
+    })
+    readonly year: number;
+
+    @IsNotEmpty({
+        message: "Runtime is required"
+    })
+    readonly runtime: number;
+
+    @IsNotEmpty({
+        message: "Genres is required"
+    })
+    readonly genres: string[];
+
+    @IsNotEmpty({
+        message: "Director is required"
+    })
+    @MaxLength(255, {
+        message: "Director cannot be longer than 255 characters"
+    })
+    readonly director: string;
+
+    readonly actors?: string;
+    readonly plot?: string;
+    readonly posterUrl?: string;
+
     constructor(
-        readonly id: number,
-        readonly title: string,
-        readonly year: string,
-        readonly runtime: number,
-        readonly genres: string[],
-        readonly director: string,
-        readonly actors?: string,
-        readonly plot?: string,
-        readonly posterUrl?: string,
-    ) {}
+        id: number,
+        title: string,
+        year: number,
+        runtime: number,
+        genres: string[],
+        director: string,
+        actors?: string,
+        plot?: string,
+        posterUrl?: string,
+    ) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.runtime = runtime;
+        this.genres = genres;
+        this.director = director;
+        this.actors = actors;
+        this.plot = plot;
+        this.posterUrl = posterUrl;
+    }
 
 }
