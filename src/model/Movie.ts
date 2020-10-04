a6761e0c4b528853b4a034c91696a0bc07d560f5
Piotr Mol <piotr94mol@gmail.com>
2020-10-04 18:21:01 +0200

    Changed Movie model

--- src/model/Movie.ts
+++ src/model/Movie.ts
@@ -1,35 +1,16 @@
 
 export default class Movie {
-    id: number;
-    title: string;
-    year: string;
-    runtime: number;
-    genres: string[];
-    director: string;
-    actors?: string;
-    plot?: string;
-    posterUrl?: string;
 
     constructor(
-        id: number,
-        title: string,
-        year: string,
-        runtime: number,
-        genres: string[],
-        director: string,
-        actors?: string,
-        plot?: string,
-        posterUrl?: string,
-    ) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.runtime = runtime;
-        this.genres = genres;
-        this.director = director;
-        this.actors = actors;
-        this.plot = plot;
-        this.posterUrl = posterUrl;
-    }
+        readonly id: number,
+        readonly title: string,
+        readonly year: string,
+        readonly runtime: number,
+        readonly genres: string[],
+        readonly director: string,
+        readonly actors?: string,
+        readonly plot?: string,
+        readonly posterUrl?: string,
+    ) {}
 
 }
