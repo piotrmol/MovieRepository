ddf7d8fdc105a8123b0a8d539eaa2fa518b273cc
Piotr Mol <piotr94mol@gmail.com>
2020-10-06 21:18:34 +0200

    Tested saveMovie from MovieService

--- tests/services/MovieService.spec.ts
+++ tests/services/MovieService.spec.ts
@@ -1,29 +1,60 @@
-import { MovieServiceImpl } from "../../src/services/MovieService";
-import { MovieRepository } from "../../src/repositories/MovieRepository";
 import Movie from "../../src/models/Movie";
-import { MovieValidatorImpl } from "../../src/utils/MovieValidator";
+import { MovieService, MovieServiceImpl } from "../../src/services/MovieService";
+import { MockedMovieRepository } from "../Mocks/MockedMovieRepository";
+import { MockedMovieValidator } from "../Mocks/MockedMoviewValidator";
 
+describe("MovieService tests", () => {
 
-class MockMovieRepository implements MovieRepository{
-    saveMovie(movie: Movie): Promise<void> {
-        return Promise.reject();
-    }
-    getAllMovies() {
+    it("Throws validation error", () => {
+        const service = getService(false, true);
+        const movie = getMovie();
+        movie.title = null;
 
-    }
+        expect(service.saveMovie(movie)).rejects.toThrow("Title is required");
+    });
 
-    getAllGeneres(): string[] {
-        return ["Comedy", "Horror", "Drama"];
-    }
-}
+    it("Throws repository error", () => {
+        const service = getService(true, false);
+        const movie = getMovie();
 
+        expect(service.saveMovie(movie)).rejects.toThrow("I/O exception");
+    });
 
-test('Getting list of all available genres', () => {
-    const service = new MovieServiceImpl(new MockMovieRepository(), new MovieValidatorImpl());
-    const genres = service.getAllGenres()
+    it("Saves movie. Nothing is returned", () => {
+        const service = getService(false, false);
+        const movie = getMovie();
 
-    expect(genres.length).toBe(3);
-    expect(genres).toContain("Comedy");
-    expect(genres).toContain("Horror");
-    expect(genres).toContain("Drama");
+        expect(service.saveMovie(movie)).resolves.toBe(undefined);
+    });
+
+    it('Gets list of three available genres', () => {
+        const service = getService(false, false);
+        const genres = service.getAllGenres();
+
+        expect(genres.length).toBe(3);
+        expect(genres).toContain("Comedy");
+        expect(genres).toContain("Horror");
+        expect(genres).toContain("Drama");
+    });
+
+    const getService = (shouldRepositoryFail: boolean, shouldValidatorFail): MovieService => {
+        const repository = new MockedMovieRepository(shouldRepositoryFail);
+        const validator = new MockedMovieValidator(shouldValidatorFail);
+        return new MovieServiceImpl(repository, validator);
+    };
+
+    const getMovie = (): Movie => {
+        const movie = new Movie();
+        movie.director = "director";
+        movie.genres = [];
+        movie.id = 1;
+        movie.runtime = 123;
+        movie.year = 123;
+        movie.title = "title";
+        return movie;
+    };
+
 });
+
+
+
