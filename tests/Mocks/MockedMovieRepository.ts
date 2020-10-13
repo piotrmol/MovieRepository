75fecec70c4ad6e8af010fe6d6549ba5a8e054ec
Piotr Mol <piotr94mol@gmail.com>
2020-10-13 10:25:35 +0200

    Implemented getAllMovies in MockedMovieRepository

--- tests/Mocks/MockedMovieRepository.ts
+++ tests/Mocks/MockedMovieRepository.ts
@@ -1,5 +1,8 @@
 import Movie from "../../src/models/Movie";
 import { MovieRepository } from "../../src/repositories/MovieRepository";
+import path from "path";
+import { FileManagerImpl } from "../../src/utils/FileManager";
+import MovieDatabase from "../../src/models/MovieDatabase";
 
 export const MockedMovieRepository = jest.fn<MovieRepository, any>((shouldFail: boolean) => ({
     saveMovie(movie: Movie): Promise<void> {
@@ -7,7 +10,9 @@ export const MockedMovieRepository = jest.fn<MovieRepository, any>((shouldFail: 
     },
 
     getAllMovies(): Movie[] {
-        return [];
+        const url = path.join(__dirname, "../Mocks/Json/MockedDb.json");
+        const fileManager = new FileManagerImpl<MovieDatabase>(url);
+        return fileManager.readContentOfFile().movies;
     },
 
     getAllGeneres(): string[] {
